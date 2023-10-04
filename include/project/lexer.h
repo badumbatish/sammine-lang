9db8b3ed4b9752ef867081224442f46188aef0a0
Jasmine T <tanghocle456@gmail.com>
2023-10-03 19:46:10 -0700

    Basic CMakeLists working

--- include/project/lexer.h
+++ include/project/lexer.h
@@ -1,10 +1,30 @@
 //
 // Created by Jasmine Tang on 10/3/23.
 //
-
+#include <string>
 #pragma once
 
 class lexer {
 
+    static std::string IdentifierStr; // Filled in if tok_identifier
+    static double NumVal;             // Filled in if tok_number
+
+
+    static int get_tok();
+
+};
+
+// The lexer returns tokens [0-255] if it is an unknown character, otherwise one
+// of these for known things.
+enum Token {
+    tok_eof = -1,
+
+    // commands
+    tok_def = -2,
+    tok_extern = -3,
+
+    // primary
+    tok_identifier = -4,
+    tok_number = -5,
 };
 
--- src/project/lexer.cpp
+++ src/project/lexer.cpp
@@ -3,3 +3,53 @@
 //
 
 #include "project/lexer.h"
+
+int lexer::get_tok() {
+    static int LastChar = ' ';
+
+    // Skip any whitespace.
+    while (isspace(LastChar))
+        LastChar = getchar();
+
+    if (isalpha(LastChar)) { // identifier: [a-zA-Z][a-zA-Z0-9]*
+        IdentifierStr = LastChar;
+        while (isalnum((LastChar = getchar())))
+            IdentifierStr += LastChar;
+
+        if (IdentifierStr == "def")
+            return Token::tok_def;
+        if (IdentifierStr == "extern")
+            return Token::tok_extern;
+        return Token::tok_identifier;
+    }
+
+    if (isdigit(LastChar) || LastChar == '.') {   // Number: [0-9.]+
+        std::string NumStr;
+        do {
+            NumStr += LastChar;
+            LastChar = getchar();
+        } while (isdigit(LastChar) || LastChar == '.');
+
+        NumVal = strtod(NumStr.c_str(), 0);
+        return Token::tok_number;
+    }
+
+    if (LastChar == '#') {
+        // Comment until end of line.
+        do
+            LastChar = getchar();
+        while (LastChar != EOF && LastChar != '\n' && LastChar != '\r');
+
+        if (LastChar != EOF)
+            return get_tok();
+    }
+
+    // Check for end of file.  Don't eat the EOF.
+    if (LastChar == EOF)
+        return Token::tok_eof;
+
+    // Otherwise, just return the character as its ascii value.
+    int ThisChar = LastChar;
+    LastChar = getchar();
+    return ThisChar;
+}
