fe70b4000bad9d9c1b7103e7db7924a5df5e934f
jjasmine <tanghocle456@gmail.com>
2024-03-07 04:22:48 -0500

    Dummy documentation for Lexer

--- include/Lexer.h
+++ include/Lexer.h
@@ -5,6 +5,10 @@
 #ifndef SAMMINE_LANG_LEXER_H
 #define SAMMINE_LANG_LEXER_H
 
+//! A lexer for sammine-lang
+
+//! Not sure what to put in here
+//! Not sure what to put in here
 class Lexer {};
 
 #endif //SAMMINE_LANG_LEXER_H
