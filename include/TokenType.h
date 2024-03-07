8f86a6fe9d0b8553b40c70a57e12acb43441aa4b
jjasmine <tanghocle456@gmail.com>
2024-03-07 04:46:16 -0500

    Add a list of tokens

--- include/TokenType.h
+++ include/TokenType.h
@@ -9,6 +9,61 @@
 /// Hi there this is a TokenType enum
 ///
 enum TokenType {
+   TokADD, // +
+   TokSUB, // -
+   TokMUL, // *
+   TokDIV, // /
+   TokMOD, // %
+
+   TokAddAssign, // +=
+   TokAddIncr,   // ++
+   TokSubAssign, // -=
+   TokSubDecr,   // --
+   TokMulAssign, // *=
+   TokDivAssign, // /=
+
+   TokAND, // &&
+   TokOR,  // ||
+   TokXOR, // ^
+   TokSHL, // <<
+   TokSHR, // >>
+
+   TokEQUAL,   // ==
+   TokLESS,    // <
+   TokGREATER, // >
+   TokASSIGN,  // =
+   TokNOT,     // !
+
+   TokAndAssign,    // &=
+   TokOrAssign,     // !=
+   TokXorAssign,    // ^=
+   TokShlAssign,    // <<=
+   TokShrAssign,    // >>=
+   TokAndNotAssign, // &^=
+
+    //TokEXP AND FloorDiv
+   TokEXP,      // **
+   TokFloorDiv, // /_
+   TokCeilDiv,  // /^
+
+    //TokPAREN
+    TokLeftParen, // (
+    TokRightParen, // )
+    TokLeftCurly, // {
+    TokRightCurly, // }
+
+    //TokFunction
+    TokFunc, // func
+
+    // TokIf
+    TokIf, // if
+    TokElse, // else
+
+    //TokCOMMENTS
+   TokSingleComment, //
+   TokEOF,
+   TokINVALID,
+
 
 };
 #endif //SAMMINE_LANG_TOKENTYPE_H
