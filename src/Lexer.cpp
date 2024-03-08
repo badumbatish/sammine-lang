5dc29a43ed2f969e428907120258840c1e38ec43
jjasmine <tanghocle456@gmail.com>
2024-03-08 04:31:34 -0500

    Working on lexing simple tokens

--- src/Lexer.cpp
+++ src/Lexer.cpp
@@ -33,17 +33,44 @@ void Lexer::consume() {
 
 }
 
-Lexer::Lexer(FileRAII file) : Lexer() {
-    std::string sub_str = {};
-    while (!file.isEOF()) {
 
-    }
-}
 
 Lexer::Lexer(const std::string& input) : Lexer() {
     int i = 0;
 
     while (i < input.length()) {
+        while(isspace(input[i])) {
+            i++;
+        }
+
+        if (isalpha(input[i])) { // identifier: [a-zA-Z][a-zA-Z0-9]*
+            std::string IdentifierStr;
+            IdentifierStr = input[i];
+
+            while (isalnum(input[++i]))
+                IdentifierStr += input[i];
+
+            if (IdentifierStr == "func")
+                TokStream.push_back(std::make_shared<Token>(Token(TokFunc, "")));
+            else if (IdentifierStr == "if")
+                TokStream.push_back(std::make_shared<Token>(Token(TokIf, "")));
+            else if (IdentifierStr == "else")
+                TokStream.push_back(std::make_shared<Token>(Token(TokElse, "")));
+            else
+                TokStream.push_back(std::make_shared<Token>(Token(TokID, IdentifierStr)));
+
+            continue;
+        }
+
+        if (isdigit(input[i]) || input[i] == '.') { // Number: [0-9.]+
+            std::string NumStr;
+            do {
+                NumStr += input[i++];
+            } while (isdigit(input[i]) || input[i] == '.');
+
+            TokStream.push_back(std::make_shared<Token>(Token(TokID, NumStr)));
+            continue;
+        }
 
     }
 
