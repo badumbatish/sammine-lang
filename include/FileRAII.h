fb003c6509d9ce9a7f7330c496981977a42afd12
jjasmine <tanghocle456@gmail.com>
2024-03-08 03:33:23 -0500

    Add a RAII file

---
+++ include/FileRAII.h
@@ -0,0 +1,47 @@
+//
+// Created by jjasmine on 3/8/24.
+//
+
+#ifndef SAMMINE_LANG_FILERAII_H
+#define SAMMINE_LANG_FILERAII_H
+#include <string>
+#include <fstream>
+#include <iostream>
+class FileRAII {
+public:
+    // Constructor opens the file
+    FileRAII(const std::string& filename) : file(filename), is_opened(true) {
+        if (!file.is_open()) {
+            std::cerr << "Error: Unable to open file: " << filename << std::endl;
+            is_opened = false;
+        }
+    }
+
+    // Destructor closes the file
+    ~FileRAII() {
+        if (is_opened) {
+            file.close();
+        }
+    }
+
+    // Function to check if the file is open
+    bool isOpen() const {
+        return is_opened;
+    }
+
+    // Function to read one character at a time
+    char readChar() {
+        char ch;
+        if (file.get(ch)) {
+            return ch;
+        } else {
+            is_opened = false;  // Mark the file as closed if there are no more characters
+            return '\0';       // Return null character to indicate end of file
+        }
+    }
+
+private:
+    std::ifstream file;
+    bool is_opened;
+};
+#endif //SAMMINE_LANG_FILERAII_H
