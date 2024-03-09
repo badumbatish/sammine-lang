0cc57d12242c5d0f786481c8323c0192de0570c9
jjasmine <tanghocle456@gmail.com>
2024-03-09 02:40:32 -0500

    Introduce new ASTVisitor class

--- include/AstBase.h
+++ include/AstBase.h
@@ -5,4 +5,21 @@
 #ifndef SAMMINE_LANG_ASTBASE_H
 #define SAMMINE_LANG_ASTBASE_H
 
+namespace sammine_lang {
+    namespace AST {
+
+        class ASTVisitor {
+
+        };
+        class Visitable {
+        public:
+            virtual ~Visitable () = default;
+            virtual void accept_vis (ASTVisitor &vis) = 0;
+        };
+        class AstBase {
+
+        };
+
+    }
+}
 #endif //SAMMINE_LANG_ASTBASE_H
