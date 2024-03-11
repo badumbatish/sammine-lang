7c64ffb56f9740e72ce400d05cfe4e6a1da9b047
Jasmine Tang <tanghocle456@gmail.com>
2024-03-11 07:44:50 +0000

    Add more comments, fix VarDefAST members

--- include/Ast.h
+++ include/Ast.h
@@ -31,22 +31,26 @@ namespace sammine_lang {
         };
 
         class DefinitionAST : AstBase {};
+
+        //! \brief A variable definition: "var x = expression;"
         class VarDefAST : DefinitionAST {
             std::shared_ptr<TypedVarAST> TypedVar;
-            double Val;
+            std::shared_ptr<ExprAST> Expression;
         };
 
-        //! \brief A prototype to present func func_name(...) -> type;
+        //! \brief A prototype to present "func func_name(...) -> type;"
 
         //!
         //!
         class PrototypeAST : AstBase {};
+
+        //! \brief A Function Definition that has the prototype and definition in terms of a block
         class FuncDefAST : DefinitionAST {
             std::shared_ptr<PrototypeAST> Prototype;
             std::shared_ptr<BlockAST> Block;
         };
 
-        //! \brief To simulate a { } code block
+        //! \brief An AST to simulate a { } code block
 
         //!
         //!
