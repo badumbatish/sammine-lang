7cd8d77dbcb213ba709347d9dece4eba520a0d6b
Jasmine Tang <tanghocle456@gmail.com>
2024-03-21 21:56:06 +0000

    Add tests about declarations

--- unit-test/test_parser.cpp
+++ unit-test/test_parser.cpp
@@ -19,17 +19,66 @@ TEST_CASE("Empty program parsing", "[Parser]") {
 }
 
 TEST_CASE("Definition parsing", "[Parser]") {
-    auto lexer = sammine_lang::Lexer("let b : blablabla = 0;");
-    auto tokStreamPtr = lexer.getTokenStream();
-    auto pg = sammine_lang::Parser(tokStreamPtr);
 
-    auto programAST = pg.Parse();
+    SECTION("Variable definition with number") {
+        auto lexer = sammine_lang::Lexer("let b : blablabla = 0;");
+        auto tokStreamPtr = lexer.getTokenStream();
+        auto pg = sammine_lang::Parser(tokStreamPtr);
+
+        auto programAST = pg.Parse();
 
 
-    REQUIRE(programAST->DefinitionVec.size() == 1);
+        REQUIRE(programAST->DefinitionVec.size() == 1);
 
-    auto varDef = dynamic_pointer_cast<sammine_lang::AST::VarDefAST>(programAST->DefinitionVec.front());
-    REQUIRE(varDef->TypedVar->name == "b");
-    REQUIRE(varDef->TypedVar->type == "blablabla");
+        auto varDef = dynamic_pointer_cast<sammine_lang::AST::VarDefAST>(programAST->DefinitionVec.front());
+        REQUIRE(varDef->TypedVar->name == "b");
+        REQUIRE(varDef->TypedVar->type == "blablabla");
+    }
+    
+    SECTION("Variable definition with binary expression") {
+        auto lexer = sammine_lang::Lexer("let b : blablabla = 1+2;");
+        auto tokStreamPtr = lexer.getTokenStream();
+        auto pg = sammine_lang::Parser(tokStreamPtr);
+
+        auto programAST = pg.Parse();
+
+
+        REQUIRE(programAST->DefinitionVec.size() == 1);
+
+        auto varDef = dynamic_pointer_cast<sammine_lang::AST::VarDefAST>(programAST->DefinitionVec.front());
+        REQUIRE(varDef->TypedVar->name == "b");
+        REQUIRE(varDef->TypedVar->type == "blablabla");
+    }
+
+    SECTION("Variable definition with call expression") {
+        auto lexer = sammine_lang::Lexer("let b : blablabla = hi();");
+        auto tokStreamPtr = lexer.getTokenStream();
+        auto pg = sammine_lang::Parser(tokStreamPtr);
+
+        auto programAST = pg.Parse();
+
+
+        REQUIRE(programAST->DefinitionVec.size() == 1);
+
+        auto varDef = dynamic_pointer_cast<sammine_lang::AST::VarDefAST>(programAST->DefinitionVec.front());
+        REQUIRE(varDef->TypedVar->name == "b");
+        REQUIRE(varDef->TypedVar->type == "blablabla");
+    }
+
+
+    SECTION("Variable definition with string literal expression") {
+        auto lexer = sammine_lang::Lexer("let b : blablabla = \"how about this\" ");
+        auto tokStreamPtr = lexer.getTokenStream();
+        auto pg = sammine_lang::Parser(tokStreamPtr);
+
+        auto programAST = pg.Parse();
+
+
+        REQUIRE(programAST->DefinitionVec.size() == 1);
+
+        auto varDef = dynamic_pointer_cast<sammine_lang::AST::VarDefAST>(programAST->DefinitionVec.front());
+        REQUIRE(varDef->TypedVar->name == "b");
+        REQUIRE(varDef->TypedVar->type == "blablabla");
+    }
 
 }
