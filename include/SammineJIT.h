1a0e42a258384bf0b278fec629314d037b6b20c8
Jasmine T <tanghocle456@gmail.com>
2024-03-29 18:41:54 -0700

    Writing a JIT because why not

---
+++ include/SammineJIT.h
@@ -0,0 +1,20 @@
+//
+// Created by Jasmine Tang on 3/29/24.
+//
+
+#ifndef SAMMINE_LANG_SAMMINEJIT_H
+#define SAMMINE_LANG_SAMMINEJIT_H
+
+#include "llvm/ADT/StringRef.h"
+#include "llvm/ExecutionEngine/Orc/CompileUtils.h"
+#include "llvm/ExecutionEngine/Orc/Core.h"
+#include "llvm/ExecutionEngine/Orc/ExecutionUtils.h"
+#include "llvm/ExecutionEngine/Orc/IRCompileLayer.h"
+#include "llvm/ExecutionEngine/Orc/JITTargetMachineBuilder.h"
+#include "llvm/ExecutionEngine/Orc/RTDyldObjectLinkingLayer.h"
+#include "llvm/ExecutionEngine/SectionMemoryManager.h"
+#include "llvm/IR/DataLayout.h"
+#include "llvm/IR/LLVMContext.h"
+
+
+#endif // SAMMINE_LANG_SAMMINEJIT_H
---
+++ src/SammineJIT.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jasmine Tang on 3/29/24.
+//
+
+#include "SammineJIT.h"
